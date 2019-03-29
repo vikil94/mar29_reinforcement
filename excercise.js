d553b92060db0086d73af2286a57a8dadc912759
Vikil Naik <vikilnaik37@gmail.com>
2019-03-29 14:13:40 -0400

    finished excercise

---
+++ excercise.js
@@ -0,0 +1,6 @@
+const tasks = ["laundry", "shovel driveway", "grocery shopping", "check email", "daydream", "phone parents", "fill prescription", "exercise", "take out recycling", "check the weather"];
+console.log("Sunday To Do List:");
+for (let i = 0; i < tasks.length; i++) {
+
+    console.log(`${i + 1}: ${tasks[i]}`);
+};
